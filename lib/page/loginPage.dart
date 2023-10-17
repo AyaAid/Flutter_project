c56183cfb88c4c0fd0c3d37f0a4e3410614aea8a
AyaAid <aya.lakhel11@gmail.com>
2023-10-17 23:32:03 +0200

    login form and home page test init

---
+++ lib/page/loginPage.dart
@@ -0,0 +1,90 @@
+import 'package:andrestable/main.dart';
+import 'package:andrestable/page/homePage.dart';
+import 'package:flutter/material.dart';
+import 'package:andrestable/database/mongodb.dart';
+
+class LoginFormModel {
+  String username = '';
+  String password = '';
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _loginForm = LoginFormModel();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Connexion'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Nom d\'utilisateur'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Veuillez entrer votre nom d\'utilisateur';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _loginForm.username = value!;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Mot de passe'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Veuillez entrer votre mot de passe';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _loginForm.password = value!;
+                },
+              ),
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    bool idValid = await MongoDataBase().verifyLog(
+                        _loginForm.username, _loginForm.password);
+
+                    if (idValid) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => HomePage()),
+                      );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Identifiants incorrects'),
+                          duration: Duration(seconds: 3),
+                        ),
+                      );
+                    }
+                  }
+                },
+                child: const Text('Se connecter'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
